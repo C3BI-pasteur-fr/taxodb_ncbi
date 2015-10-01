3f6b6a2ded53ae18a1946c68749c5ddf9201eb3b
maufrais <maufrais@pasteur.fr>
2015-10-01 17:01:59 +0200

    T: setu.py file midification C: install_requires added

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@ from distutils.core import setup
 
 setup(name = "taxodb_ncbi",
     version = "2.0",
-    description = "",
+    description = (""),
+    license='',
     author = "Corinne Maufrais",
     author_email = "corinne.maufrais@pasteur.fr",
     url = "",
     package_dir={'': 'src'},
     packages=[''],
+    install_requires = ['golden >= 2', 
+	'bdb', 
+],
 ) 
