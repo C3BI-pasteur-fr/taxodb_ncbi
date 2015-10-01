4c5eea3d0ff6d3c8a3d533133f57de2f59bbf11c
maufrais <maufrais@pasteur.fr>
2015-10-01 16:44:54 +0200

    T: Creation of setup.py C: Very simple --> must to be completed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name = "taxodb_ncbi",
+    version = "2.0",
+    description = "",
+    author = "Corinne Maufrais",
+    author_email = "corinne.maufrais@pasteur.fr",
+    url = "",
+    package_dir={'': 'src'},
+    packages=[''],
+) 
