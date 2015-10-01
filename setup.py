0fd20d8a05ad8137a3fe6b89dea5d078500ccd26
maufrais <maufrais@pasteur.fr>
2015-10-01 17:07:27 +0200

    T: setu.py modification C: add golden version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name = "taxodb_ncbi",
     url = "",
     package_dir={'': 'src'},
     packages=[''],
-    install_requires = ['golden >= 2', 
+    install_requires = ['golden >= 3.0, 
 	'bdb', 
 ],
 ) 
